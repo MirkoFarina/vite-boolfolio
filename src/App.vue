050d1a0711a1d642ae661469098b1eefcc8fbc86
MirkoFarina <MirkoFarina@outlook.com>
2023-01-26 16:27:46 +0100

    [add] call to db, whit axios and print in console

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'app',
+    data(){
+        return {
+            baseUrl: 'http://127.0.0.1:8000/api/',
+            projects: []
+        }
+    },
+    methods: {
+        getApi(){
+            axios.get(this.baseUrl + 'projects')
+                  .then(risultato => {
+                     console.log(risultato.data);
+                  } )
+        }
+    },
+    mounted(){
+        this.getApi()
+    }
+}
+</script>
+
 <template>
   
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style lang="scss">
     @use './style/general.scss';
